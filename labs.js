3d877d03e02189f7204745e641c428492083ecf0
Ben Jaffe <ben@udacity.com>
2015-05-15 14:07:20 -0700

    remove unneeded content

--- labs.js
+++
@@ -1,7 +0,0 @@
-$('.labs-list-item-content').each(function(i, elem){
-	$(elem).click(function(e){
-		if (e.target.tagName !== 'A' && $(elem).attr('data-href') !== '#') {
-			window.open($(elem).attr('data-href'), '_blank');
-		}
-	});
-});
